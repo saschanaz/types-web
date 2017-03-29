aa630884500968ac5838a193fd0285e830e90b03
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-03-29 10:14:57 +0900

    remove unneeded type conversion
    1. AbortMode and CanvasPixelArray do not exist anymore
    2. EndOfStreamError is defined as an enum type, which can already be
    managed by `EmitEnums`.

--- TS.fsx
+++ TS.fsx
@@ -693,15 +693,12 @@ module Emit =
     /// Get typescript type using object dom type, object name, and it's associated interface name
     let rec DomTypeToTsType (objDomType: string) =
         match objDomType.Trim('?') with
-        | "AbortMode" -> "String"
         | "any" -> "any"
         | "bool" | "boolean" | "Boolean" -> "boolean"
-        | "CanvasPixelArray" -> "number[]"
         | "Date" -> "Date"
         | "DOMHighResTimeStamp" -> "number"
         | "DOMString" -> "string"
         | "DOMTimeStamp" -> "number"
-        | "EndOfStreamError" -> "number"
         | "EventListener" -> "EventListenerOrEventListenerObject"
         | "double" | "float" | "unrestricted float" -> "number"
         | "Function" -> "Function"
